6b46a8de0a2f1c265c246c7889a08f10e0ad871b
Lloyd Park <llpark30@gmail.com>
2017-11-02 14:27:43 -0400

    extract character images

--- preproc.py
+++ preproc.py
@@ -1,4 +1,4 @@
-import numpy   as np
+import numpy  as np
 import scipy.misc # to visualize only\
 from skimage import measure
 import cv2
@@ -21,19 +21,33 @@ def findcharacters(image):
     for x in range(len(contours)):
         areas[x] = cv2.contourArea(contours[x])
     threelargestareas = sorted(areas, reverse=True)[:3]
+    charactercoord = []
 
     for contour in contours:
         area = cv2.contourArea(contour)
         if area in threelargestareas:
             (x,y,w,h) = cv2.boundingRect(contour)
+            charactercoord.append((x,y,w,h))
             cv2.rectangle(image, (x,y), (x+w,y+h), (255, 255, 255), 1)
+    return charactercoord
 
+def getcharimages(image, charactercoord):
+    images = []
+    for (x,y,w,h) in charactercoord:
+        images.append(image[y:y+h,x:x+w])
+    return images
 
-lines = openfile('train_x.csv')
-image = linetoImage(lines[28])
-scipy.misc.imshow(image)
+def resizeimage(image):
+    return scipy.misc.imresize(image, (32,32))
 
-findcharacters(image)
-scipy.misc.imshow(image)
+def main():
+    lines = openfile('train_x.csv')
+    image = linetoImage(lines[28])
+    charcoords = findcharacters(image)
+    charimages = getcharimages(image, charcoords)
+    for x in charimages:
+        scipy.misc.imshow(resizeimage(x))
 
-#im2 = findobjects()
+    scipy.misc.imshow(image)
+
+if __name__ == "__main__": main()
