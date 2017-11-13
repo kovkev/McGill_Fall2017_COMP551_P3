c0584bbea0d02a4b7eff216777354f1489266b5a
lloydpark <llpark30@gmail.com>
2017-11-13 18:42:58 -0500

    Add files via upload

--- logreg.py
+++ logreg.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import numpy as np
 import time
 import csv
@@ -10,7 +11,7 @@ with open("train_x.csv") as csvfile:
     reader = csv.reader(csvfile, quoting = csv.QUOTE_NONNUMERIC)
     for row in reader:
         row = np.array(row).astype(float).astype(np.uint8)
-        row[row>240] = 255
+        row[row>240] = 1
         row[row<=240] = 0
         train_x.append(row)
 
@@ -24,20 +25,29 @@ with open("test_x.csv") as csvfile:
     reader = csv.reader(csvfile, quoting = csv.QUOTE_NONNUMERIC)
     for row in reader:
         row = np.array(row).astype(float).astype(np.uint8)
-        row[row>240] = 255
+        row[row>240] = 1
         row[row<=240] = 0
         test_x.append(row)
 
 
 regr = linear_model.LogisticRegression()
-
 train_y = np.array(train_y)
 train_y = train_y.ravel()
 print "fitting the model"
 print time.clock()
-regr.fit(train_x[0:10000], train_y[0:10000])
+regr.fit(train_x, train_y)
 print time.clock()
 print "model training complete"
+
+# validation = regr.predict(train_x[40000:50000])
+#
+# counter = 0;
+# for x,y in zip(validation, train_y[40000:50000]):
+#     if x == y:
+#         counter += 1
+# print ("counter is: %s" % counter)
+# validationaccuracy = counter/len(validation)
+# print ("validation accuracy: %s" % validationaccuracy)
 
 test_y = regr.predict(test_x)
 
@@ -45,6 +55,6 @@ file = open('test_y.csv', 'w')
 file.write("Id,label\n")
 counter = 0
 for output in test_y:
-    counter += 1
-    file.write("%s,%s\n" % (counter, output))
+   counter += 1
+   file.write("%s,%s\n" % (counter, output))
 file.close()
